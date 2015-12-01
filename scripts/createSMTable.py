5a718f7b7843a5dbadf2b69421e354cf66bd08c2
hverlin <hugues.verlin@gmail.com>
2015-12-01 16:51:59 +0100

    Mise à jour de la table des services + commentaires IHM

--- scripts/createSMTable.py
+++ scripts/createSMTable.py
@@ -6,18 +6,18 @@
 # PRINT HEAD
 print("");
 print("\\begin{table}[H]");
-print("\t\\centering");
-print("\t\\resizebox{\\columnwidth}{!}{");
-print("\t\t\\begin{tabular}{p{1cm}|p{5cm}p{6cm}p{6cm}}");
-print("\t\t\tNuméro SM & Nom & Arguments & Valeur de retour \\\\ \\hline");
+print("\t\\noindent\\resizebox{15cm}{!}{");
+print("\t\t\\begin{tabular}{P{0.8cm}P{5.5cm}P{6cm}P{6cm}}");
+print("\\hline Num & Nom & Arguments & Valeur de retour \\\\ \\hline");
 # PRINT CONTENT
 COUNTER=0;
 with open('../diagram/variables.puml', 'r') as f:
     for l in f:
         if "!define SM_" in l:
-            print("\t\t\t%s & %s & %s & %s \\\\ \\hline" % (COUNTER, l.split(' ')[2].split('(')[0].replace("\\n", ""), l.split('(')[1].split(')')[0].replace("\\n", " "), l.split(':')[1].replace('\n','').replace("\\n", "")));
+            print("\t\t\t%s & %s & %s & %s \\\\ \\arrayrulecolor{lightgray}\\hline" % (COUNTER, l.split(' ')[2].split('(')[0].replace("\\n", ""), l.split('(')[1].split(')')[0].replace("\\n", " "), l.split(':')[1].replace('\n','').replace("\\n", "")));
             COUNTER+=1;
 # PRINT END
+print("\\arrayrulecolor{black}\\hline")
 print("\t\t\\end{tabular}");
 print("\t}");
 print("\t\\caption{Tableau de synthèse des SMA}");
